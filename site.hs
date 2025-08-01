766a613a7289e853b8ac7c9aaf4a57beb98abf19
rekk <rekk@gmx.de>
2025-08-01 12:01:09 +0200

    Update typography

--- site.hs
+++ site.hs
@@ -7,7 +7,7 @@ import Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match "assets/*" $ do
         route idRoute
         compile copyFileCompiler
 
